b500c81e31ef74aafb446cd41903fb67e97ac22d
camilo-gc <camilogarcia.dev@gmail.com>
2023-12-02 08:23:42 -0500

    initial commit

---
+++ src/main/java/com/example/apiusermonitoring/ApiUserMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiusermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUserMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiusermonitoring/ApiUserMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiusermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
